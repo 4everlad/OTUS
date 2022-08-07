c589d26f3bcf8719de928d5e43df538de02194bb
4everlad <d.s.bakulin@gmail.com>
2022-08-07 16:16:20 +0300

    Initial Commit

---
+++ OTUS/OTUSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTUSApp.swift
+//  OTUS
+//
+//  Created by Dmitry Bakulin on 07.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct OTUSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
